6d9b79fdd0182239d3fcf8710461b30f212954bb
Leanne <leannestranks86@hotmail.com>
2020-12-21 17:53:33 +1000

    Event keycode created

--- Dad_Jokes/script.js
+++ Dad_Jokes/script.js
@@ -5,16 +5,31 @@ jokeBtn.addEventListener("click", generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             "Accept": "application/json"
+//         }
+//     }
+
+//     fetch("https://icanhazdadjoke.com", config)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         })
+// }
+
+
+// async/await 
+async function generateJoke() {
     const config = {
         headers: {
             "Accept": "application/json"
         }
     }
 
-    fetch("https://icanhazdadjoke.com", config)
-        .then((response) => response.json())
-        .then((data) => {
-            jokeEl.innerHTML = data.joke
-        })
+    const res = await fetch("https://icanhazdadjoke.com", config)
+
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke = data.joke
 }
